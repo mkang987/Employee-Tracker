504ce08ac4b5bebcd6a8305391093ff7993d1954
Moon Kang <mkang987@gmail.com>
2022-03-18 11:59:37 -0400

    Added base files and set up schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+)
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    deparment_id INT,
+
+    FOREIGN KEY (deparment_id) REFERENCES department(id) ON DELETE SET NULL
+)
+
+CREATE TABLE employee (
+    id INT PRIMARY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+)
